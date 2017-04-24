1ed0ca304b6fc8fa158bf10149cac94c9e002e1d
Kali Nuska <KaliNuska@users.noreply.github.com>
2017-04-24 20:29:50 +0200

    Update Personas.py

--- TIC/Personas.py
+++ TIC/Personas.py
@@ -1,111 +1,112 @@
-class Persona:
-  def __init__(self):
-    self.nombre = None
-    self.sexo = None
-    self.edad = None
-
-  def __str__(self):
-    return(' ' + str.capitalize(self.nombre) + ', ' + str.upper(self.sexo) + ', ' + str(self.edad) + ' años.')
-
-  def preguntar_valores(self):
-    self.nombre = input('Nombre ' + str(i+1) + ': ')
-    while str.isalpha(self.nombre) == False:
-      print('El nombre no es válido. Introduzca sólo caracteres alfabéticos')
-      self.nombre = input('Nombre ' + str(i+1) + ': ')
-
-    self.sexo = input('Sexo ' + str(i+1) + ': ')
-    while (len(self.sexo) > 1) or (str.upper(self.sexo) != 'V') & (str.upper(self.sexo) != 'M'):
-      print('Dato incorrecto. Introduzca solo un caracter (V, M)')
-      self.sexo = input('Sexo ' + str(i+1) + ': ')
-
-    self.edad = input('Edad ' + str(i+1) + ': ')
-    while str.isdigit(self.edad) == False:
-      print('La edad debe ser un número entero de años.')
-      self.edad = input('Edad ' + str(i+1) + ': ')
-    self.edad = int(self.edad)
-
-
-
-n = input("¿Cuántas personas quiere analizar?\n")
-while str.isdigit(n) == False or int(n) < 1:
-  print('El número de personas debe ser un número natural.')
-  n = input("¿Cuántas personas quiere analizar?\n")
-n = int(n)
-
-array = []
-
-for i in range(n):
-
-  temp = Persona()
-  temp.preguntar_valores()
-
-  array.append(temp)
-
-
-maximo = 0
-minimo = 99
-
-mayor = []
-menor = []
-
-for person in array:
-  if person.edad > maximo:
-    maximo = person.edad
-
-  if person.edad < minimo:
-    minimo = person.edad
-
-for person in array:
-  if person.edad == maximo:
-    mayor.append(person)
-  if person.edad == minimo:
-    menor.append(person)
-
-print('\nLa persona más mayor es:\n')
-for person in mayor:
-  print(person)
-print('\nLa persona más joven es:\n')
-for person in menor:
-  print(person)
-
-
-def ordenar_burbuja(coleccion):
-  n = len(coleccion)
-  i = 0
-  while i < n:
-    j = i
-    while j < n:
-      if coleccion[i].edad > coleccion[j].edad:
-        temp = coleccion[i]
-        coleccion[i] = coleccion[j]
-        coleccion[j] = temp
-      j += 1
-    i += 1
-
-
-ordenar_burbuja(array)
-
-print('\nLas personas ordenadas de menor a mayor edad:\n')
-for a in array:
-  print(a)
-
-
-def discriminar(listado):
-  hom = []
-  muj = []
-  for persona in listado:
-    if str.upper(persona.sexo) == 'V':
-      hom.append(persona)
-    elif str.upper(persona.sexo) == 'M':
-      muj.append(persona)
-  print('\nHombres:\n')
-  for persona in hom:
-    print(persona)
-  print('\nMujeres:\n')
-  for persona in muj:
-    print(persona)
-
-
-discriminar(array)
-
-input('\n-- Pulse ENTER para salir --')
+# Gestión de personas mediante tres atributos: Nombre, edad y sexo.
+# Prestaciones: Mayores, menores, orden por edad y discriminación por sexo.
+class Persona:                                                                                                 # Creación de la clase persona. Métodos:
+  def __init__(self):                                                                                          # - Constructor: inicializa los atributos en blanco
+    self.nombre = None                                                                                         # 
+    self.sexo = None                                                                                           # 
+    self.edad = None                                                                                           # 
+                                                                                                               # 
+  def toString(self):                                                                                          # - Dar formato al imprimir el objeto. De esta forma, en vez 
+    return(' ' + str.capitalize(self.nombre) + ', ' + str.upper(self.sexo) + ', ' + str(self.edad) + ' años.') #    de que python devuelva la ubicación en la memoria del
+                                                                                                               #    objeto, sale un texto personalizado.
+  def preguntar_valores(self):                                                                                 # - Preguntar valores con los que rellenar los atributos.
+    self.nombre = input('Nombre ' + str(i+1) + ': ')                                                           #    Leer entrada para cada nombre.
+    while str.isalpha(self.nombre) == False:                                                                   #    Validar entrada según:
+      print('El nombre no es válido. Introduzca sólo caracteres alfabéticos')                                  #     Caracteres alfabéticos, sin espacios ni puntuación.
+      self.nombre = input('Nombre ' + str(i+1) + ': ')                                                         #    Una vez validado queda almacenado el nombre.
+                                                                                                               # 
+    self.sexo = input('Sexo ' + str(i+1) + ': ')                                                               #    Leer entrada para el sexo de cada persona.
+    while (len(self.sexo) > 1) or (str.upper(self.sexo) != 'V') & (str.upper(self.sexo) != 'M'):               #    Validar entrada según:
+      print('Dato incorrecto. Introduzca solo un caracter (V, M)')                                             #     m, M, V ó v.
+      self.sexo = input('Sexo ' + str(i+1) + ': ')                                                             #    Una vez validado queda almacenado el sexo.
+                                                                                                               # 
+    self.edad = input('Edad ' + str(i+1) + ': ')                                                               #    Leer entrada para cada edad.
+    while str.isdigit(self.edad) == False:                                                                     #    Validar entrada según:
+      print('La edad debe ser un número entero de años.')                                                      #     Número entero mayor o igual a 0.
+      self.edad = input('Edad ' + str(i+1) + ': ')                                                             #    Una vez validado queda almacenada la edad.
+    self.edad = int(self.edad)                                                                                 #    Además se convierte a tipo 'entero'.
+                                                                                                               # 
+                                                                                                               # 
+n = input("¿Cuántas personas quiere analizar?\n")                                                              # 'n' guarda el número de personas a analizar
+while str.isdigit(n) == False or int(n) < 1:                                                                   # Validar 'n' según:
+  print('El número de personas debe ser un número natural.')                                                   #  Número entero positivo.
+  n = input("¿Cuántas personas quiere analizar?\n")                                                            # Una vez validado se almacena en 'n'.
+n = int(n)                                                                                                     # 'n' se convierte a tipo 'entero'.
+                                                                                                               # 
+array = []                                                                                                     # Creación de una lista vacía, 'array', que contendrá
+                                                                                                               #  a todas las personas.
+for i in range(n):                                                                                             # Bucle que guardará los datos de las 'n' personas.
+                                                                                                               # 
+  temp = Persona()                                                                                             # Objeto temp de la clase Persona.
+  temp.preguntar_valores()                                                                                     # Se le dan valores a los atributos de temp.
+                                                                                                               # 
+  array.append(temp)                                                                                           # Se mete temp en el array de personas.
+                                                                                                               # 
+                                                                                                               # 
+maximo = 0                                                                                                     # El menor mayor valor posible es 0.
+minimo = 99                                                                                                    # El mayor menor valor posible es 99.
+                                                                                                               # 
+mayor = []                                                                                                     # Para sacar todas las personas con la edad más alta
+menor = []                                                                                                     #  y más baja, creo un array de los mayores y otro de
+                                                                                                               #  los menores.
+for person in array:                                                                                           # Buscar la edad más alta de entre todas las personas.
+  if person.edad > maximo:                                                                                     #  Si es más alta que la anterior más alta, se
+    maximo = person.edad                                                                                       #   reemplaza por la nueva alta.
+                                                                                                               # 
+  if person.edad < minimo:                                                                                     #  Si es más baja que la anterior más baja, se
+    minimo = person.edad                                                                                       #   reemplaza por la nueva baja.
+                                                                                                               # 
+for person in array:                                                                                           # Almacenar en los array mayor y menor a las personas
+  if person.edad == maximo:                                                                                    #  que tienen la edad más alta y baja (encontradas en
+    mayor.append(person)                                                                                       #  el paso anterior).
+  if person.edad == minimo:                                                                                    # 
+    menor.append(person)                                                                                       # 
+                                                                                                               # 
+print('\nLa persona más mayor es:\n')                                                                          # Texto.
+for person in mayor:                                                                                           # Como puede haber más de una persona que tenga la
+  print(person.toString())                                                                                     #  máxima edad, necesito un bucle para sacarlas todas.
+print('\nLa persona más joven es:\n')                                                                          # Texto.
+for person in menor:                                                                                           # Como puede haber más de una persona que tenga la
+  print(person.toString())                                                                                     #  mínima edad, necesito un bucle para sacarlas todas.
+                                                                                                               # 
+                                                                                                               # 
+def ordenar_burbuja(coleccion):                                                                                # Defino una función para ordenar utilizando el
+  n = len(coleccion)                                                                                           #  algoritmo burbuja. 'n' será ahora el total de
+  i = 0                                                                                                        #  elementos a ordenar.
+  while i < n:                                                                                                 # Se ordena sustituyendo uno por el anterior si se
+    j = i                                                                                                      #  da que uno es menor que el anterior.
+    while j < n:                                                                                               # 
+      if coleccion[i].edad > coleccion[j].edad:                                                                # 
+        temp = coleccion[i]                                                                                    # 
+        coleccion[i] = coleccion[j]                                                                            # 
+        coleccion[j] = temp                                                                                    # 
+      j += 1                                                                                                   # 
+    i += 1                                                                                                     # 
+                                                                                                               # 
+                                                                                                               # 
+ordenar_burbuja(array)                                                                                         # Llamar a la función de ordenar
+                                                                                                               # 
+print('\nLas personas ordenadas de menor a mayor edad:\n')                                                     # Texto.
+for a in array:                                                                                                # Sacar todas las personas del array, que ya ha sido
+  print(a.toString())                                                                                          #  ordenado.
+                                                                                                               # 
+                                                                                                               # 
+def discriminar(listado):                                                                                      # Defino una función para separar hombres de mujeres
+  hom = []                                                                                                     #  en distintos array: 'hom' y 'muj'. La función
+  muj = []                                                                                                     #  también imprime el resultado.
+  for persona in listado:                                                                                      # Para todas las personas del parámetro 'listado'.
+    if str.upper(persona.sexo) == 'V':                                                                         # Si el sexo introducido es v o V, se añade a la
+      hom.append(persona)                                                                                      #  lista de hom.
+    elif str.upper(persona.sexo) == 'M':                                                                       # Si el sexo introducido es m o M, se añade a la
+      muj.append(persona)                                                                                      #  lista de muj.
+  print('\nHombres:\n')                                                                                        # Texto.
+  for persona in hom:                                                                                          # Imprime todos los hombres de hom.
+    print(persona.toString())                                                                                  # 
+  print('\nMujeres:\n')                                                                                        # Texto.
+  for persona in muj:                                                                                          # Imprime todas las mujeres de muj.
+    print(persona.toString())                                                                                  # 
+                                                                                                               # 
+                                                                                                               # 
+discriminar(array)                                                                                             # Lalar a la función de deiscriminar pasando el
+                                                                                                               #  contenido de array como parámetro por referencia.
+input('\n-- Pulse ENTER para salir --')                                                                        # Confirmación de salida
