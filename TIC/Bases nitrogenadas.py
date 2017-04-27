27914469158924a4b0863a7f5287c6ec1062c2bd
Kali Nuska <KaliNuska@users.noreply.github.com>
2017-04-27 22:34:05 +0200

    Update Bases nitrogenadas.py

--- TIC/Bases nitrogenadas.py
+++ TIC/Bases nitrogenadas.py
@@ -1,25 +1,30 @@
 # Complementación de bases nitrogenadas:
-dict = {'A':'T','T':'A','a':'t','t':'a','C':'G','G':'C','c':'g','g':'c'}
-chain = input('Introduzca la cadena de bases nitrogenadas:\n')
-for i in range(len(chain)):
-    if chain[i] in dict:
-      print(dict[chain[i]],end='')
+Base = {'A':'T','T':'A','a':'t','t':'a','C':'G','G':'C','c':'g','g':'c'} # Establecer el diccionario con los pares complementarios.
+chain = input('Introduzca la cadena de bases nitrogenadas:\n')           # Lectura de la cadena de entrada.
+
+for i in range(len(chain)):                                              # Bucle de tantas iteraciones como caracteres se han introducido.
+    if chain[i] in Base:                                                 # Si el caracter del índice i está registrado en el diccionario,
+      print(Base[chain[i]],end='')                                       # Se imprime su definición, en este caso, su base complementaria.
+
+#############################################################################################################################################
 
 # Versión cíclica:
-c = 's'
-dict = {'A':'T','T':'A','a':'t','t':'a','C':'G','G':'C','c':'g','g':'c'}
+c = 's'                                                                  # Cuando c valga n deja de repetirse el programa
+Base = {'A':'T','T':'A','a':'t','t':'a','C':'G','G':'C','c':'g','g':'c'} # Establecer el diccionario con los pares complementarios
 
-while c != 'n':
-  chain = input('Introduzca la cadena de bases nitrogenadas:\n')
-  print('La cadena complementaria es:')
+while c != 'n':                                                          # Repetir hasta que el usuario indique con 'n' que no desea repetir.
+  chain = input('Introduzca la cadena de bases nitrogenadas:\n')         # Lectura de la cadena de entrada, guardada en 'chain'
+  print('La cadena complementaria es:')                                  # Texto
 
-  for i in range(len(chain)):
-    if chain[i] in dict:
-      print(dict[chain[i]],end='')
+  for i in range(len(chain)):                                            # Bucle de tantas iteraciones como caracteres contenga la cadena.
+    if chain[i] in Base:                                                 # Si el caracter del iterador i está registrado en el diccionario,
+      print(Base[chain[i]],end='')                                       # Se imprime su definición, es decir, su base complementaria.
 
-  c = input('\n¿Quiere introducir otra cadena? (s/n)\n')
+  c = input('\n¿Quiere introducir otra cadena? (s/n)\n')                 # Confirmar salida del programa
 
-# Versión mecánica:
+#############################################################################################################################################
+
+# Versión mecánica. Va comprobando que el caracter sea una base y la cambia por su complementaria:
 c = 's'
 
 while c != 'n':
